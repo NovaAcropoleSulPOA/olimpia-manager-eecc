d47bf1007a5e44e4b3bea11a6850235b93770b95
Olimar Teixeira Borges <olimarbjunior@gmail.com>
2025-01-22 07:20:45 +0200

    credenciais supabase

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://your-project-url.supabase.co';
-const supabaseAnonKey = 'your-anon-key';
+const supabaseUrl = 'https://usppufmcuywelqzzedjs.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVzcHB1Zm1jdXl3ZWxxenplZGpzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzc1MTQyMDIsImV4cCI6MjA1MzA5MDIwMn0.8xQOvTELWYf3XcKeaql-GPJl9eKn-KSg4VnWZ8Bg02Q';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
