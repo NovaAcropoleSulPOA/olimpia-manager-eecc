c198762ec63cbb07c34689de2f863534aef0580b
Olimar Teixeira Borges <olimarbjunior@gmail.com>
2025-01-30 01:32:52 -0300

    Ajuste de quebra de linha no título da página

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -60,7 +60,7 @@ const LandingPage = () => {
               </div>
 
               <h1 className="text-4xl md:text-6xl font-bold mb-4 text-center lg:text-left">
-                <span>Olimpíadas Estaduais da Escola do Esporte com Coração (EECC) </span> <br /> <span>Edição</span> <br /> <span>Porto Alegre/RS</span>
+                <span>Olimpíadas Estaduais da Escola do Esporte com Coração (EECC) </span> <br /> <span>-</span> <br /> <span>Edição:</span> <br /> <span>Porto Alegre/RS</span>
               </h1>
               <p className="text-xl md:text-2xl italic mb-12 text-center lg:text-left">
                 "Mais rápidos, mais altos, mais fortes, estamos unidos!"
